4422c97a442279e7c07ef04e807be220ea70345b
M. Adam Kendall <linuxbozo@gmail.com>
2011-10-25 10:12:14 -0400

    Update with my stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+
+# --- Functions --- #
+# Notice title
+function notice { echo  "\033[1;32m=> $1\033[0m"; }
+#
+# Error title
+function error { echo "\033[1;31m=> Error: $1\033[0m"; }
+
 cd "$(dirname "$0")"
-git pull
+notice "Updating.."
+git pull upstream master
+git push
+git submodule init
+git submodule update
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 fi
 source "$HOME/.bash_profile"
+notice "Done.."
